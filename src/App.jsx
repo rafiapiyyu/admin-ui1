7cc9d35550e92ff601def37ce462f255fc577dd5
rafiapiyyu <rafiapiyyu@gmail.com>
2026-04-22 20:52:12 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="bg-blue-100">Vite + React</h1>
+      <h1>Vite + React</h1>
+      <h3> Rafi Abiyyu Ramadhan </h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
