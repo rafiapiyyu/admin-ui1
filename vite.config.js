ef21dc412f5664f7fb0cfefbb4d4f3415be29b11
rafiapiyyu <rafiapiyyu@gmail.com>
2026-04-28 00:45:40 +0700

    membuat page sign in

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'  // ← ini harus ada
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),  // ← ini harus ada
+  ],
 })
